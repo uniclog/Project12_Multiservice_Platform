5b1405f8a1d1d4467d3c30f015d1162f8905480c
birewallg <g.sstrav@gmail.com>
2021-09-08 23:17:21 +0300

    add Teamspeak lib, create bot service

---
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak_utils/service/TeamspeakBotService.java
@@ -0,0 +1,5 @@
+package local.ts3snet.unicbot_ms_spring.module_teamspeak_utils.service;
+
+public interface TeamspeakBotService {
+
+}
---
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak_utils/service/impl/TeamspeakBotServiceImpl.java
@@ -0,0 +1,6 @@
+package local.ts3snet.unicbot_ms_spring.module_teamspeak_utils.service.impl;
+
+import local.ts3snet.unicbot_ms_spring.module_teamspeak_utils.service.TeamspeakBotService;
+
+public class TeamspeakBotServiceImpl implements TeamspeakBotService {
+}

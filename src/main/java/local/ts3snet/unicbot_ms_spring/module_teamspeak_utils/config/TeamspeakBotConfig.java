27217cfaef8ee696d8511b1edebfc0725b540dd4
birewallg <g.sstrav@gmail.com>
2021-09-08 22:22:29 +0300

    add TeamspeakBotConfig: configuration constants

---
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak_utils/config/TeamspeakBotConfig.java
@@ -0,0 +1,20 @@
+package local.ts3snet.unicbot_ms_spring.module_teamspeak_utils.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@Configuration
+@PropertySource("classpath:application.properties")
+public class TeamspeakBotConfig {
+    @Value("${teamspeak.bot.login}")
+    private String login;
+    @Value("${teamspeak.bot.password}")
+    private String password;
+    @Value("${teamspeak.bot.server.ip_address}")
+    private String ipAddress;
+    @Value("${teamspeak.bot.server.port}")
+    private String port;
+}

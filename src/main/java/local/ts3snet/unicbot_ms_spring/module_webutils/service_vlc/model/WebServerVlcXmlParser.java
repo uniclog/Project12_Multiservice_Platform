73442b5c21730254b64cc74e3f85736239b53a82
erlidione <g.sstrav@gmail.com>
2021-09-23 02:29:47 +0900

    UMP-T-3 | vlc XML parser minor fix

--- src/main/java/local/ts3snet/unicbot_ms_spring/module_webutils/service_vlc/model/WebServerVlcXmlParser.java
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_webutils/service_vlc/model/WebServerVlcXmlParser.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;

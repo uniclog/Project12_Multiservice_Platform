308e88c3cbd6b37d86907a7e64abe4e893be74da
erlidione <g.sstrav@gmail.com>
2021-10-08 17:14:37 +0900

    UMP-T-2 | add schedule event (update channel title)

--- src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak/service/impl/utils/ScheduledEvents.java
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak/service/impl/utils/ScheduledEvents.java
@@ -3,6 +3,7 @@ package local.ts3snet.unicbot_ms_spring.module_teamspeak.service.impl.utils;
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.api.ChannelProperty;
 import local.ts3snet.unicbot_ms_spring.module_teamspeak.service.TeamspeakBotService;
+import local.ts3snet.unicbot_ms_spring.module_webutils.service_vlc.service.VlcControlService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Component;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.github.theholywaffle.teamspeak3.api.ChannelProperty.CHANNEL_NAME;
 
@@ -27,6 +30,8 @@ public class ScheduledEvents {
 
     // @Autowired TeamspeakBotService
     private final TeamspeakBotService teamspeakBotServiceA;
+    // @Autowired TeamspeakBotService
+    private final VlcControlService vlcControlService;
 
     private TS3Api api;
 
@@ -50,5 +55,38 @@ public class ScheduledEvents {
         api.editChannel(22, options);
 
         log.debug(" --> time update");
+    }
+
+    /**
+     * Task for radio data update (rate = 15 sec.)
+     * Set radio info channels tittle = track now
+     */
+    private String station = "unknown";
+    private String track = "unknown";
+    @Scheduled(fixedRate = 15000, initialDelay = 10000)
+    public void radioInfoUpdate() {
+        String temp = "Station: " + vlcControlService.getStationName()
+                .replaceAll("[^0-9A-Za-zа-яА-Я_ -]", "");
+        if (temp.length() >40) temp = temp.substring(0, 39);
+        if (!temp.equals(station)){
+            Map<ChannelProperty, String> optionsC1 = new EnumMap<>(ChannelProperty.class);
+            optionsC1.put(CHANNEL_NAME, temp);
+            api.editChannel(33, optionsC1);
+            station = temp;
+            if (!station.equals("Station:"))
+                log.info("Station: " + station);
+        }
+        temp = "Track: " + vlcControlService.getTrackName()
+                .replaceAll("[^0-9A-Za-zа-яА-Я_ -]", "")
+                .replace("amp", "");
+        if (temp.length() >40) temp = temp.substring(0, 39);
+        if (!temp.equals(track)){
+            Map<ChannelProperty, String> optionsC1 = new EnumMap<>(ChannelProperty.class);
+            optionsC1.put(CHANNEL_NAME,  temp);
+            api.editChannel(31, optionsC1);
+            track = temp;
+            if (!track.equals("Track:  -"))
+                log.info(track);
+        }
     }
 }

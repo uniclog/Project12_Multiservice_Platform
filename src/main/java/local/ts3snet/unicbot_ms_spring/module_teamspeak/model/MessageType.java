369f6a4a3947c141a5a47e0b4969526b9bea3fbd
erlidione <g.sstrav@gmail.com>
2021-10-08 23:32:56 +0900

    UMP-T-35 | add MessageType enum for ts

--- src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak/model/MessageType.java
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak/model/MessageType.java
@@ -1,58 +1,26 @@
 package local.ts3snet.unicbot_ms_spring.module_teamspeak.model;
 
 public enum MessageType {
-    DEFAULT {
-        @Override
-        public String toString() {
-            return "default";
+    DEFAULT("default"),
+    SUBSCRIBER ("!sub"),
+    STATION ("!station"),
+    TRACK ("!track"),
+    NEXT ("!next"),
+    PREV("!prev"),
+    GOTO("!goto");
+
+    private final String value;
+    MessageType(String value) {
+        this.value = value;
+    }
+    public static MessageType getType(String msg) {
+        for (MessageType e : MessageType.values()) {
+            if (e.value.equalsIgnoreCase(msg))
+                return e;
         }
-    },
-    MESSAGE_FOR_ALL_SUBSCRIBERS {
-        @Override
-        public String toString() {
-            return "messageForAllSubscribers";
-        }
-    },
-    SUB {
-        @Override
-        public String toString() {
-            return "/sub";
-        }
-    },
-    UNSUB {
-        @Override
-        public String toString() {
-            return "/unsub";
-        }
-    },
-    TRACK {
-        @Override
-        public String toString() {
-            return "/track";
-        }
-    },
-    STATION {
-        @Override
-        public String toString() {
-            return "/station";
-        }
-    },
-    NEXT {
-        @Override
-        public String toString() {
-            return "/next";
-        }
-    },
-    PREV {
-        @Override
-        public String toString() {
-            return "/prev";
-        }
-    },
-    PLAY_BY_ID {
-        @Override
-        public String toString() {
-            return "/goto";
-        }
+        return DEFAULT;
+    }
+    public String getTextValue() {
+        return this.value;
     }
 }

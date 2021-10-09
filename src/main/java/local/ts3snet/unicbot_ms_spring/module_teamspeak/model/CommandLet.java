02e4752f440d660455029694de071e7aae6009f4
erlidione <g.sstrav@gmail.com>
2021-10-09 13:23:35 +0900

    UMP-T-34 | UMP-T-36 | fix CommandLet bean name

--- src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak/model/CommandLet.java
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_teamspeak/model/CommandLet.java
@@ -8,7 +8,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Data
-@Component
+@Component("moduleTeamspeakCommandLet")
 @Scope("prototype")
 public class CommandLet {
     private MessageType cmd = MessageType.DEFAULT;

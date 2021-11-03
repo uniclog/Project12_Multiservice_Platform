08b8203c257aa6573d87efd58bce114b515b0356
erlidione <g.sstrav@gmail.com>
2021-11-04 00:15:46 +0300

    UMP-T-47 | add MyFitness random messages

--- src/main/java/local/ts3snet/unicbot_ms_spring/module_webutils/service_myfitness/service/impl/MyFitnessServiceImpl.java
+++ src/main/java/local/ts3snet/unicbot_ms_spring/module_webutils/service_myfitness/service/impl/MyFitnessServiceImpl.java
@@ -1,6 +1,7 @@
 package local.ts3snet.unicbot_ms_spring.module_webutils.service_myfitness.service.impl;
 
 import local.ts3snet.unicbot_ms_spring.module_telegram.service.TelegramBotService;
+import local.ts3snet.unicbot_ms_spring.module_webutils.service_myfitness.model.MyFitnessMessagesGenerator;
 import local.ts3snet.unicbot_ms_spring.module_webutils.service_myfitness.service.MyFitnessService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,22 +9,42 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class MyFitnessServiceImpl implements MyFitnessService {
     private TelegramBotService botService;
+
     @Autowired
     public void setUnicBotTORGTelegramBotService(@Qualifier("uniclogMyFitnessTelegramBotServiceImpl") TelegramBotService bot) {
         this.botService = bot;
     }
 
+    private MyFitnessMessagesGenerator messagesGenerator;
+
+    @Autowired
+    public void setUnicBotTORGTelegramBotService(MyFitnessMessagesGenerator messagesGenerator) {
+        this.messagesGenerator = messagesGenerator;
+    }
+
     @Scheduled(cron = "0 0 10-22 * * *")
     public void everyHourNotify() {
-        botService.sendMessageForAllSubscribers("Воду пьем! хорошо живем!");
+        botService.sendMessageForAllSubscribers(
+                messagesGenerator.generate(
+                        List.of(
+                                "Воду пьем! хорошо живем!",
+                                "Время воды!",
+                                "А вы, уже пили воду?!",
+                                "Пора пить воду")));
     }
 
     @Scheduled(cron = "0 0 17 * * *")
     public void everyFitnessNotify() {
-        botService.sendMessageForAllSubscribers("Пора качать попу!");
+        botService.sendMessageForAllSubscribers(
+                messagesGenerator.generate(
+                        List.of(
+                                "Пора качать попу!",
+                                "Пора заниматься!")));
     }
 }
